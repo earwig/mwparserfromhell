4b5b8d305ffc70450bca1a2bb47dbb7c9f4143ff
Ben Kurtovic <ben.kurtovic@verizon.net>
2012-07-26 21:44:31 -0400

    Implement all methods of unicode.

--- mwparserfromhell/string_mixin.py
+++ mwparserfromhell/string_mixin.py
@@ -80,8 +80,122 @@ class StringMixIn(object):
             return unicode(item) in unicode(self)
         return item in unicode(self)
 
+    def capitalize(self):
+        return unicode(self).capitalize()
+
+    def center(self, width, fillchar=None):
+        return unicode(self).center(width, fillchar)
+
+    def count(self, sub=None, start=None, end=None):
+        return unicode(self).count(sub, start, end)
+
+    def decode(self, encoding=None, errors=None):
+        return unicode(self).decode(encoding, errors)
+
+    def encode(self, encoding=None, errors=None):
+        return unicode(self).encode(encoding, errors)
+
+    def endswith(self, prefix, start=None, end=None):
+        return unicode(self).endswith(prefix, start, end)
+
+    def expandtabs(self, tabsize=None):
+        return unicode(self).expandtabs(tabsize)
+
+    def find(self, sub=None, start=None, end=None):
+        return unicode(self).find(sub, start, end)
+
+    def format(self, *args, **kwargs):
+        return unicode(self).format(*args, **kwargs)
+
+    def index(self, sub=None, start=None, end=None):
+        return unicode(self).index(sub, start, end)
+
+    def isalnum(self):
+        return unicode(self).isalnum()
+
+    def isalpha(self):
+        return unicode(self).isalpha()
+
+    def isdecimal(self):
+        return unicode(self).isdecimal()
+
+    def isdigit(self):
+        return unicode(self).isdigit()
+
+    def islower(self):
+        return unicode(self).islower()
+
+    def isnumeric(self):
+        return unicode(self).isnumeric()
+
+    def isspace(self):
+        return unicode(self).isspace()
+
+    def istitle(self):
+        return unicode(self).istitle()
+
+    def isupper(self):
+        return unicode(self).isupper()
+
+    def join(self, iterable):
+        return unicode(self).join(iterable)
+
+    def ljust(self, width, fillchar=None):
+        return unicode(self).ljust(width, fillchar)
+
+    def lower(self):
+        return unicode(self).lower()
+
+    def lstrip(self, chars=None):
+        return unicode(self).lstrip(chars)
+
+    def partition(self, sep):
+        return unicode(self).partition(sep)
+
     def replace(self, old, new, count):
         return unicode(self).replace(old, new, count)
 
+    def rfind(self, sub=None, start=None, end=None):
+        return unicode(self).rfind(sub, start, end)
+
+    def rindex(self, sub=None, start=None, end=None):
+        return unicode(self).rindex(sub, start, end)
+
+    def rjust(self, width, fillchar=None):
+        return unicode(self).rjust(width, fillchar)
+
+    def rpartition(self, sep):
+        return unicode(self).rpartition(sep)
+
+    def rsplit(self, sep=None, maxsplit=None):
+        return unicode(self).rsplit(sep, maxsplit)
+
+    def rstrip(self, chars=None):
+        return unicode(self).rstrip(chars)
+
+    def split(self, sep=None, maxsplit=None):
+        return unicode(self).split(sep, maxsplit)
+
+    def splitlines(self, keepends=None):
+        return unicode(self).splitlines(keepends)
+
+    def startswith(self, prefix, start=None, end=None):
+        return unicode(self).startswith(prefix, start, end)
+
     def strip(self, chars=None):
         return unicode(self).strip(chars)
+
+    def swapcase(self):
+        return unicode(self).swapcase()
+
+    def title(self):
+        return unicode(self).title()
+
+    def translate(self, table, deletechars=None):
+        return unicode(self).translate(table, deletechars)
+
+    def upper(self):
+        return unicode(self).upper()
+
+    def zfill(self, width):
+        return unicode(self).zfill(width)
