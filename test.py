a24821057c7a55a06aee6048e14d71f3daad11a0
Riamse <andrewwang43@gmail.com>
2012-08-20 19:41:18 -0700

    Forgot to add compat.py

---
+++ test.py
@@ -0,0 +1,14 @@
+﻿import mwparserfromhell
+text = "foo {{spam|eggs}} bar"
+code = mwparserfromhell.parse(text)
+template = code.filter_templates()[0]
+template.name
+template.params
+template.params[0].value
+template.params[0].name
+template.params[0].showkey
+template.params[0].showkey = True
+template.params[0].name = "apples"
+code
+template.add("pears", "{{plums}}")
+code.filter_templates(recursive=True)
