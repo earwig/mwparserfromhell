ad03f60140e011dc1f47d3813693e6f4cea604d8
Merlijn van Deen <valhallasw@gmail.com>
2014-06-28 11:11:11 +0200

    Add build requirements

--- scripts/win_build.py
+++ scripts/win_build.py
@@ -1,3 +1,25 @@
+# Build requirements:
+#
+# Python 2.6-3.2: Visual C++ Express Edition 2008:
+#                 http://go.microsoft.com/?linkid=7729279
+#
+# Python 3.3+: Visual C++ Express Edition 2010:
+#              http://go.microsoft.com/?linkid=9709949
+#
+# x64 builds: Microsoft Windows SDK for Windows 7 and .NET Framework 3.5 SP1:
+#             http://www.microsoft.com/en-us/download/details.aspx?id=3138
+#
+# Python interpreter, 2.6, 2.7, 3.2-3.4:
+# https://www.python.org/downloads/
+#
+# Pip, setuptools, wheel:
+# https://bootstrap.pypa.io/get-pip.py
+# and run *for each* Python version:
+# c:\pythonXX\python get-pip.py
+# c:\pythonXX\scripts\pip install wheel
+#
+# Afterwards, run this script with any of the python interpreters (2.7 suggested)
+
 from __future__ import print_function
 import os
 from subprocess import call, STDOUT
