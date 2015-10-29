4c89e51faba4d8898c39ee8924dee68d36081af5
Ben Kurtovic <ben.kurtovic@gmail.com>
2015-10-28 22:46:36 -0500

    Fix Python version parsing.

--- scripts/win_wrapper.cmd
+++ scripts/win_wrapper.cmd
@@ -23,14 +23,8 @@ SET COMMAND_TO_RUN=%*
 SET WIN_SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
 SET WIN_WDK=c:\Program Files (x86)\Windows Kits\10\Include\wdf
 
-:: Extract the major and minor versions, and allow for the minor version to be
-:: more than 9.  This requires the version number to have two dots in it.
 SET MAJOR_PYTHON_VERSION=%PYTHON_VERSION:~0,1%
-IF "%PYTHON_VERSION:~3,1%" == "." (
-    SET MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2,1%
-) ELSE (
-    SET MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2,2%
-)
+SET MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2%
 
 :: Based on the Python version, determine what SDK version to use, and whether
 :: to set the SDK for 64-bit.
