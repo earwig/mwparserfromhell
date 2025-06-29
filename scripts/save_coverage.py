e946c5f175514da282cd502d7e0f6ea76890f957
Ben Kurtovic <ben@benkurtovic.com>
2025-06-29 04:38:44 -0400

    Try to get coveralls working again

---
+++ scripts/save_coverage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import traceback
+import uuid
+
+try:
+    if os.getenv("RUNNER_OS") == "Linux":
+        os.makedirs("/output", exist_ok=True)
+        shutil.move(".coverage", f"/output/.coverage.{uuid.uuid4().hex}")
+except Exception:
+    traceback.print_exc()
