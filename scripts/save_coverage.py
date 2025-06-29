2a04a68930ad6529b63f4a6f62403f6f44e31932
Ben Kurtovic <ben@benkurtovic.com>
2025-06-29 05:25:25 -0400

    Try to get coveralls working again

--- scripts/save_coverage.py
+++ scripts/save_coverage.py
@@ -8,6 +8,6 @@ import uuid
 try:
     if os.getenv("RUNNER_OS") == "Linux":
         os.makedirs("/output", exist_ok=True)
-        shutil.move(".coverage", f"/output/coverage.{uuid.uuid4().hex}")
+        shutil.move("coverage.lcov", f"/output/coverage.{uuid.uuid4().hex}.lcov")
 except Exception:
     traceback.print_exc()
