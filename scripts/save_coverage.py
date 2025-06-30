8ac8a66c0135fe670a67116e339728e0b0e7162f
Ben Kurtovic <ben@benkurtovic.com>
2025-06-29 21:35:01 -0400

    Fix coverage action and add pre-commit hook action

--- scripts/save_coverage.py
+++ scripts/save_coverage.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 import os
-import shutil
+import re
 import traceback
 import uuid
+from pathlib import Path
 
 try:
     if os.getenv("RUNNER_OS") == "Linux":
+        coverage = Path("coverage.lcov").read_text()
+        coverage = re.sub(
+            r"^(SF:).*?/site-packages/", r"\1src/", coverage, flags=re.MULTILINE
+        )
         os.makedirs("/output", exist_ok=True)
-        shutil.move("coverage.lcov", f"/output/coverage.{uuid.uuid4().hex}.lcov")
+        Path(f"/output/.coverage.{uuid.uuid4().hex}.lcov").write_text(coverage)
 except Exception:
     traceback.print_exc()
